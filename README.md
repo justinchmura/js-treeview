085116a229d261aba808cbd5af896372a65d9c14
Justin Chmura <justin@purechat.com>
2016-04-05 09:21:30 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,71 @@
+# Vanilla JavaScript TreeView
+
+[![Build Status](https://travis-ci.org/justinchmura/js-treeview.svg?branch=master)](https://travis-ci.org/justinchmura/js-treeview)[![npm version](https://badge.fury.io/js/js-treeview.svg)](https://badge.fury.io/js/js-treeview)
+
+A stupid, simple tree view writtin with vanilla JS. I needed a lightweight control that just displayed data in a tree form and out popped this. I wrote a [post on my blog](http://justinchmura.com/2014/07/03/javascript-tree-view/) that goes into more depth.
+
+## Dependencies
+
+None. I built this using only plain JavaScript so there's no external dependencies. Other than the CSS required for styling.
+
+## Example Usage
+
+If used outside of NPM/require, it will attach a global `TreeVew` object to `window`. If done using NPM, then it can be included using `require` like everything else. To install it via NPM:
+
+```bash
+npm install js-treeview
+```
+
+### HTML
+
+```html
+<div id="tree"></div>
+```
+
+### JavaScript
+
+```js
+
+// NPM
+var TreeView = require('js-treeview');
+
+var tree = new TreeView([
+    { name: 'Item 1', children: [] },
+    { name: 'Item 2', children: [
+            { name: 'Sub Item 1', children: [] },
+            { name: 'Sub Item 2', children: [] }
+        ]
+    }
+], 'tree');
+```
+
+## Options
+
+| Name | Type | Description |
+| ---- | ---- | ----------- |
+| `data` | `array` | The array of items to populate the tree with. Each item is required to have a `name` and a `children` array. |
+| `id` | `string` | ID of the DOM element to render tree in. |
+
+## Events
+
+| Name  | Arguments | Description |
+| ----- | --------- | ----------- |
+| `expand` | `target` - The DOM node that initiated the expand<br>`leaves` - Array of leaf DOM nodes under the target | Fires when a leaf is expanded. |
+| `expandAll` | No arguments | Fires after the `expandAll` method is called. |
+| `collapse` | `target` - The DOM node that initiated the collapse<br>`leaves` - Array of leaf DOM nodes under the target | Fires when a leaf is collapsed. |
+| `collapseAll` | No arguments | Fires after the `collapseAll` method is called. |
+| `select` | `target` - The DOM node selected<br>`data` - Data node associated with the selected element | Fires when a outermost leaf is selected. Contains data item of the leaf selected. |
+
+### Usage
+
+```js
+tree.on('select', function (e) {
+    console.log(JSON.stringify(e));
+});
+```
+
+[CodePen Example](http://codepen.io/justinchmura/pen/PZzBOP/)
+
+## License
+
+This plugin is available under [the MIT license](http://mths.be/mit).
---
+++ test/test-runner.js
@@ -0,0 +1,89 @@
+(function ($) {
+  'use strict';
+
+  var tree;
+  var data = [
+    { name: 'Vegetables', children: [] },
+    { name: 'Fruits', children: [
+        { name: 'Apple', children: [] },
+        { name: 'Orange', children: [] },
+        { name: 'Lemon', children: [] }
+      ]
+    },
+    { name: 'Candy', children: [
+        { name: 'Gummies', children: [] },
+        { name: 'Chocolate', children: [
+            { name: 'M & M\'s', children: [] },
+            { name: 'Hershey Bar', children: [] }
+          ]
+        }
+      ]
+    },
+    { name: 'Bread', children: [] }
+  ];
+
+  function setup() {
+    tree = new TreeView(data, 'treeview');
+  }
+
+  module('init', { setup: setup });
+
+  test('creates leaf for each item', function () {
+    var leafCount = $('.tree-leaf-text').length;
+    equal(leafCount, 11, 'Proper amount of leaves');
+  });
+
+  test('creates expando for each item with children', function () {
+    var expandoCount = $('.tree-expando').not('.hidden').length;
+    equal(expandoCount, 3, 'Proper amount of expandos');
+  });
+
+  module('events', { setup: setup });
+
+  asyncTest('emits "expand" event when leaf is expanded', function () {
+    var $expando = $('.tree-leaf-text:contains("Candy")').first().siblings('.tree-expando');
+    $expando.click(); // Collapse it first
+    tree.on('expand', function (e) {
+      ok(true, 'Expand emitted');
+      start();
+    });
+    $expando.click(); // Expand leaf
+  });
+
+  asyncTest('emits "collapse" event when leaf is collapsed', function () {
+    var $expando = $('.tree-leaf-text:contains("Candy")').first().siblings('.tree-expando');
+    tree.on('collapse', function (e) {
+      ok(true, 'Collapse emitted');
+      start();
+    });
+    $expando.click(); // Collapse leaf
+  });
+
+  asyncTest('emits "select" event when a leaf is clicked', function () {
+    var $leaf = $('.tree-leaf-text:contains("Orange")');
+    tree.on('select', function (e) {
+      ok(true, 'Select emitted');
+      equal(e.data.name, 'Orange', 'Proper leaf clicked');
+      equal(e.data.children.length, 0, 'No children on leaf');
+      start();
+    });
+    $leaf.click();
+  });
+
+  asyncTest('emits "expandAll" event when expandAll method is called', function () {
+    tree.on('expandAll', function () {
+      ok(true, 'expandAll emitted');
+      start();
+    });
+    tree.expandAll();
+  });
+
+  asyncTest('emits "collapseAll" event when collapseAll method is called', function () {
+    tree.on('collapseAll', function () {
+      ok(true, 'collapseAll emitted');
+      start();
+    });
+    tree.collapseAll();
+  });
+
+}(window.jQuery));
